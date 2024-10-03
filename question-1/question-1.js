541bf40009464986d2809e17486004b401d7adcb
harry <harry@HARRY>
2024-10-03 20:25:38 +0500

    Added README and QUESTION 1 folder and js

---
+++ question-1/question-1.js
@@ -0,0 +1,11 @@
+/// MO HARRY BANDUKDA ///
+/// STUDENT ID: 101451857 ///
+/// LAB TEST ONE - COMP2123 ///
+
+
+// Create a script with a function named lowerCaseWords that takes a mixed array as input.
+// The function will do the following.
+//  - return a promise that is resolved or rejected
+//  - filter the non-strings and lower case the remaining words
+
+
