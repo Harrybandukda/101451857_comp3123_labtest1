1becb4c814892cf0ca0ea82b2ab27d6bfa831acb
harry <harry@HARRY>
2024-10-03 20:59:30 +0500

    Question One Completed

--- question-1/question-1.js
+++ question-1/question-1.js
@@ -8,4 +8,31 @@
 //  - return a promise that is resolved or rejected
 //  - filter the non-strings and lower case the remaining words
 
+function lowerCaseWords(array) {
+    return new Promise((resolve, reject) => {
+        const lowercasedStr  = array
+        .filter(array => typeof array === 'string')
+        .map (array => array.toLowerCase());
 
+        (lowercasedStr) 
+            ? resolve(lowercasedStr) 
+            : reject('Opps: String is not found in th array');
+    })
+    
+}
+
+const mixedArray = ['PIZZA', 10, true, 25, false, 'Wing']
+const mixedArray2 = ['HArRY', 'PrItEsH', false, 25, 'pATEL', 'BANDUKDA'] 
+const mixedArray3 = [false, 25,] 
+
+lowerCaseWords(mixedArray)
+    .then(result => console.log(result))    
+    .catch(error => console.log(error))
+
+lowerCaseWords(mixedArray2)
+    .then(result => console.log(result))    
+    .catch(error => console.log(error))
+
+lowerCaseWords(mixedArray3)
+    .then(result => console.log(result))    
+    .catch(error => console.log(error))
